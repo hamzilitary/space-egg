237a7ab672ce2a36f05edf5e4ad048160ada6a68
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-03-16 13:41:57 -0700

    pass first test and fail second test

--- spec/space-age.spec.js
+++ spec/space-age.spec.js
@@ -1,8 +1,14 @@
-import {SpaceAge} from "../src/space-age"
+import {SpaceAge} from "../src/space-age.js"
 
 describe('SpageAge', function() {
+  it('should return age', function () {
+    let testAge = new SpaceAge(18);
+    expect(testAge.age).toEqual(18)
+  })
+
   it('should return age in seconds', function () {
     let testAge = new SpaceAge(18);
-    expect(testAge).toEqual(568034265)
+    let ageInSec = testAge.makeSeconds();
+    expect(ageInSec).toEqual(568034265)
   })
 })
--- src/space-age.js
+++ src/space-age.js
@@ -1,8 +1,10 @@
 export class SpaceAge {
-  constructor (age) {
-    this.age = age
-    this.birthDate
+  constructor(age) {
+    this.age = age;
   }
 
-
+  makeSeconds(){
+    let secondsAge = "";
+    return secondsAge;
+  }
 }
